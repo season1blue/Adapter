9ef37fc7f8c1a49eafb6a417df6231234acbf50e
season1blue <510519588@qq.com>
2024-12-21 15:15:02 +0800

    init msg

---
+++ eval_gqa.py
@@ -0,0 +1,96 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+from memvp.build import create_model
+import torch
+from dataclasses import dataclass
+from memvp.tokenizer import Tokenizer
+import json
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+from torchvision.transforms import transforms as T
+from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+import warnings
+
+from tqdm import tqdm
+
+transform  = T.Compose(
+            [T.Resize((224, 224), interpolation=Image.BICUBIC), 
+             T.ToTensor(),
+             T.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD)])
+
+@dataclass
+class ModelArgs_7B:
+    llama_model_path = './data/weights/'
+    llm_model = '7B'
+    max_seq_len = 512
+    hidden_proj = 128
+    emb = 320
+    cpu_load = False
+    adapter_scale = 0.1
+    adapter_dim = 12
+    gradient_checkpointing = False
+    is_train = False
+    image_root='/ai/teacher/dkc/Assets/GQA/images'
+
+args = ModelArgs_7B()
+llama = create_model(args)
+
+
+adapter = torch.load('/ai/teacher/dkc/sub/MemVP-G-dual/GQA9M-dual/checkpoint-0.pth')['model'] #  
+
+
+sd = {}
+for k in adapter:
+    sd[k.replace('module.', '')] = adapter[k]
+llama.load_state_dict(sd, False)
+
+tokenizer = Tokenizer(model_path=os.path.join(args.llama_model_path, 'tokenizer.model'))
+
+
+# data = json.load(open('/ai/teacher/dkc/Assets/GQA/jsons/random_20000.json'))
+data = json.load(open('/ai/teacher/dkc/Assets/GQA/jsons/balanced_evalset_list.json')) 
+count = 0
+correct = 0
+total = len(data)
+
+class EvalSet(Dataset):
+    def __init__(self):
+        self.data = json.load(open('/ai/teacher/dkc/Assets/GQA/jsons/balanced_evalset_list.json'))
+        self.img_root = '/ai/teacher/dkc/Assets/GQA/images'
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        item = self.data[idx]
+        question = item['question']
+        answer = item['answer']
+        img_id = item['imageId']
+        indicator = 1 if img_id is not None else 0
+        img = Image.open(f'{self.img_root}/{img_id}.jpg').convert('RGB')
+
+        w, h = img.size
+        half_size = (h//2, w//2)
+        hf_img = T.Resize(half_size, interpolation=Image.BICUBIC)(img)
+
+        hf_img = transform(hf_img)
+        img = transform(img)
+        _format_q = f"Question: {question}?\nResponse:The answer is"
+        return _format_q, answer, img, hf_img, indicator
+
+eval_set = EvalSet()
+dataloader = DataLoader(dataset=eval_set, batch_size=64)
+count = 0
+correct = 0
+flag = False
+for qs, answsers, imgs, hf_imgs, indicators in tqdm(dataloader):
+    preds = llama.generate(qs, imgs, hf_imgs, indicators, 20, tokenizer)
+    for idx, pred in enumerate(preds):
+        count += 1
+        if count == 2000:
+            flag = True
+        print(f'pred: {pred:<20} gt: {answsers[idx]:<20} {correct:>5}/{count:<5} acc: {float(correct)/count * 100:2.2f}% {total}')
+        if pred == answsers[idx]:
+            correct += 1
+    if flag:
+        break
+    print(f'{correct}/{count}')
---
+++ util/datasets.py
@@ -0,0 +1,99 @@
+import json, random
+import torch.utils.data as Data
+from torchvision.transforms import transforms
+import os
+from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+from PIL import Image
+from util.base_prompt import *
+import torch
+from memvp import Tokenizer
+import copy
+from dataclasses import dataclass
+
+class GQA(Data.Dataset):
+    @dataclass
+    class Args:
+        question_file = "/ai/teacher/dkc/Assets/GQA/jsons/balanced_trainset.json"
+        # question_file = "/ai/teacher/dkc/Assets/GQA/jsons/random_20000.json"
+        image_path = "/ai/teacher/dkc/Assets/GQA/images" # 用全局的dataset
+        max_words = 512
+        max_image_feats = 1
+        model_path = "./data/weights" # 要加载tokenizer的
+
+    def __init__(self):
+        super(GQA, self).__init__()
+        self.args = self.Args
+        self.tokenizer = Tokenizer(model_path=self.args.model_path + '/tokenizer.model')
+        self.max_words = self.args.max_words
+        self.max_image_feats = self.args.max_image_feats
+
+        self.image_path = self.args.image_path
+        self.data = json.load(open(self.args.question_file))
+
+        print(f"number of problems:  {len(self.data)}\n")
+
+        self.transforms = transforms.Compose(
+            [transforms.Resize((224, 224), interpolation=Image.BICUBIC), 
+             transforms.ToTensor(),
+             transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD)])
+
+    def tokenize(self, prompt, answer):
+        example = prompt + answer
+        prompt = torch.tensor(self.tokenizer.encode(prompt, bos=True, eos=False), dtype=torch.int64)
+        example = torch.tensor(self.tokenizer.encode(example, bos=True, eos=True), dtype=torch.int64)
+        padding = self.max_words - example.shape[0]
+        if padding > 0:
+            example = torch.cat((example, torch.zeros(padding, dtype=torch.int64) - 1))
+        elif padding < 0:
+            example = example[:self.max_words]
+        labels = copy.deepcopy(example)
+        labels[:len(prompt)] = -1
+        example_mask = example.ge(0)
+        label_mask = labels.ge(0)
+        example[~example_mask] = 0
+        labels[~label_mask] = 0
+        example_mask = example_mask.float()
+        label_mask = label_mask.float()
+        return example, labels, example_mask, label_mask
+
+    def build_prompt(self, question, answer):
+        _format_question = f'Question: {question}\nResponse:'
+        _format_answer = f"The answer is {answer}"
+        return _format_question, _format_answer
+
+    def __getitem__(self, idx):
+        item = self.data[idx]
+        question = item['question']
+        imageId = item['imageId']
+        answer = item['answer']
+
+        prompt_question, prompt_answer = self.build_prompt(question, answer)
+        
+        if imageId is not None:
+            image_path = os.path.join(self.args.image_path, f"{imageId}.jpg")
+            image = Image.open(image_path).convert('RGB')
+            
+            w, h = image.size
+            half_size = (h//2, w//2)
+            half_image = transforms.Resize(half_size, interpolation=Image.BICUBIC)(image)
+            half_image = self.transforms(half_image)
+
+
+            image = self.transforms(image)
+            indicator = 1
+        else:
+            image = torch.Tensor(torch.zeros(3, 224, 224).float())
+            half_image = torch.Tensor(torch.zeros(3, 224, 224).float())
+            indicator = 0
+
+        example, labels, example_mask, label_mask = self.tokenize(prompt_question, prompt_answer)
+        return example, labels, example_mask, image, half_image, indicator
+
+    def __len__(self):
+        return len(self.data)
+
+    def shuffle_list(self, list):
+        random.shuffle(list)
+
+
+
