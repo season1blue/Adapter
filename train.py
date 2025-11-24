c918c2d8640c7d58351c7f91e596b8329d9d91d2
season1blue <510519588@qq.com>
2025-11-24 23:02:46 +0800

    update

--- train.py
+++ train.py
@@ -231,7 +231,7 @@ def main(args):
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
     print('Training time {}'.format(total_time_str))
-
+    print()
 
 if __name__ == '__main__':
 
